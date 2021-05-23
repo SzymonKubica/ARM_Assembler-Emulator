6b1a9ff482340a7cc305398e0c01edebc175199a
Szymon Kubica <szymon.kubica20@imperial.ac.uk>
2021-05-23 21:36:11 +0200

    Added a skeleton for the reading of the binary files.

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,22 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
+	
+	/* The memory has 64KB (65536 bytes), but it is 4 bytes aligned, 
+	** so we need 65536 / 4 = 16384 memory locations each 4 bytes long.
+	** each locations stores 32 bits which is equal to sizeof(int).
+	*/
+	static const int wordsInMemory = 16384;
+	int *memory = calloc(wordsInMemory, sizeof(int));
+
+	if (argc < 2) {
+		printf("\n Error. \n No binary file provided.");
+		return -1;
+	}
+
+	FILE *binaryInput = fopen(argv[1], "rb");
+	fread(&memory, sizeof(int), wordsInMemory, binaryInput);
+
   return EXIT_SUCCESS;
 }
