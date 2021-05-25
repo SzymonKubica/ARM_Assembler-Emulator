cdb305b73fc37abb6c685c6b2b5d2cf49a9a2915
Szymon Kubica <szymon.kubica20@imperial.ac.uk>
2021-05-25 20:50:09 +0200

    Added a library for converting binary strings into bytes.

---
+++ src/defns.h
@@ -0,0 +1,7 @@
+/*
+ * This file contains all of the definitions used accross the project.
+ */
+
+typedef unsigned char Byte;
+
+
---
+++ src/lib/binaryString.h
@@ -0,0 +1,8 @@
+#include "../defns.h"
+
+/*
+ * Binary String module: a tool for operating on bytes in a visual way.
+ */
+
+extern Byte readBinary(char *str);
+extern int length(char *str);
