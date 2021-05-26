b350f5cf5c1c129d3be6fc5c80a6c5f53a6e87e7
Oscar Garcia Velasco Newell <og519@texel01.doc.ic.ac.uk>
2021-05-26 14:37:49 +0100

    Multiply get Functions Complete + Edit to common.c to abstract binary operations

--- src/emulator/multiply/multiply.c
+++ src/emulator/multiply/multiply.c
@@ -1,3 +1,32 @@
+#include <stdio.h> 
+#include "../../defns.h"
+#include "binaryString.h"
+#include "common.h"
+
+byte_t get_Rn_multiply (byte_t thirdByte) {
+	return get_First_Nibble(thirdByte); 
+}
+
+byte_t get_Rd_multiply (byte_t secondByte) {
+	return get_Second_Nibble(secondByte);
+}
+
+byte_t get_Rs (byte_t thirdByte) {
+	return get_Second_Nibble(thirdByte); 
+}
+
+byte_t get_Rm (byte_t fourthByte) {
+	return get_Second_Nibble(fourthByte);
+}
+
+byte_t get_A (byte_t secondByte) {
+	return get_First_Nibble(secondByte) >> 1;
+}
+
+byte_t get_S (byte_t secondByte) {
+	return get_First_Nibble(secondByte) & 1;
+}
+
 int main(void) {
-	// Dummy for compilation.
+	// DUMMY
 }
