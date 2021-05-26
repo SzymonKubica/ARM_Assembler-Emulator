e09de572e669cf57191929521866b8e425b9bec9
Oscar Garcia Velasco Newell <og519@texel01.doc.ic.ac.uk>
2021-05-26 21:05:16 +0100

    Remove unecessary length function from binaryString lib file, replace with strlen (string.h)

--- src/lib/binaryString.c
+++ src/lib/binaryString.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "../defns.h"
+#include <string.h>
 
 /*
  * Binary String module: implementation.
@@ -11,19 +12,9 @@
  * Pre: the number is less than or equal 8 bits.
  */
 
-
-// NOT NECESSARY: use strlen(str) funciton
-int length(char *str) {
-	int length = 0;
-	for (; *str != '\0'; str++) {
-		length++;
-	}
-	return length;
-}
-
 byte_t readBinary(char *str) {
 	byte_t byte = 0x0;
-	for (int i = length(str), j = -1; i >= 0; i--, j++) {
+	for (int i = strlen(str), j = -1; i >= 0; i--, j++) {
 		if (*(str + i) == '1') {
 			byte += (1 << j);
 		}
