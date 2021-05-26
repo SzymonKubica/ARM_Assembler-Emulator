6eb602aed168e8e6426629f7303d38da5dca888e
Oscar Garcia Velasco Newell <og519@texel01.doc.ic.ac.uk>
2021-05-26 14:43:35 +0100

    Removing compiled code

--- src/emulator/common_functions/common.h
+++ src/emulator/common_functions/common.h
@@ -1,6 +1,10 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+byte_t get_First_Nibble (byte_t byte);
+
+byte_t get_Second_Nibble (byte_t byte); 
+
 byte_t get_Rn (byte_t secondByte);
 
 byte_t get_Rd (byte_t thirdByte);
