bfc3452b6a8eae2cd5afa43e379dc66ed79693e9
Oscar Garcia Velasco Newell <og519@texel01.doc.ic.ac.uk>
2021-05-26 16:05:04 +0100

    Execute multiply function complete

--- src/emulator/multiply/multiply.c
+++ src/emulator/multiply/multiply.c
@@ -27,6 +27,28 @@ byte_t get_S (byte_t secondByte) {
 	return get_First_Nibble(secondByte) & 1;
 }
 
+void set_CPSR (word_t result, word_t *cpsr) {
+	*cpsr &= 0x7fffffff;
+	*cpsr |= (result >> 31) << 31;
+	if(!result) {
+        	*cpsr |= 0x40000000;
+	}	       
+}
+
+void execute_multiply (byte_t *firstByte, word_t *registers) {
+	if (get_A(firstByte[1])) {
+		registers[get_Rd(firstByte[1])] 
+			= get_Rm(firstByte[3]) * get_Rs(firstByte[2]) + get_Rn(firstByte[2]);
+	} else {
+		registers[get_Rd(firstByte[1])]
+			= get_Rm(firstByte[3]) * get_Rs(firstByte[2]); 
+	}
+	
+	if (get_S(firstByte[1])) {
+		set_CPSR(registers[get_Rd(firstByte[1])], &registers[16]);
+	}
+}
+
 int main(void) {
-	// DUMMY
+	return 0; 
 }
