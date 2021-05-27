82563c3259fd3aa7a487242e34443bdeca189755
Szymon Kubica <szymon.kubica20@imperial.ac.uk>
2021-05-27 11:29:31 +0200

    Single data processing has been updated

--- src/emulator/single_data_processing/single_data_processing.c
+++ src/emulator/single_data_processing/single_data_processing.c
@@ -36,38 +36,50 @@ unsigned short get_offset(byte_t thirdByte, byte_t fourthByte,
 	}
 }
 
-static byte_t add_offset(byte_t Rn, byte_t offset) {
-	return Rn + offset;	
+// Helper functions for execute_single_data_transfer.
+static void execute_pre_indexing(
+		byte_t *firstByte, 
+		word_t *registers, 
+		byte_t*memory);
+
+static void execute_post_indexing(
+		byte_t *firstByte, 
+		word_t *registers, 
+		byte_t*memory);
+
+static byte_t apply_offset(byte_t Rn, byte_t *firstByte, word_t *registers);
+
+static void change_base_register_by_offset(
+		byte_t Rn, 
+		byte_t *firstByte, 
+		word_t *registers);
+
+static void load(byte_t Rn, byte_t Rd, word_t *registers, byte_t *memory);
+static void store(byte_t Rn, byte_t Rd, word_t *registers, byte_t *memory);
+static byte_t add_offset(byte_t Rn, byte_t offset);
+static byte_t subtract_offset(byte_t Rn, byte_t offset);
+
+void execute_single_data_transfer (byte_t *firstByte, word_t *registers, byte_t *memory) {
+	if (get_pre_post_indexing_bit(firstByte[1])) {
+
+		execute_pre_indexing(firstByte, registers, memory);
+
+	} else {
+
+		execute_post_indexing(firstByte, registers, memory);
+
+	}
 }
 
-static byte_t subtract_offset(byte_t Rn, byte_t offset) {
-	return Rn - offset;
-}
-
-static void load(byte_t Rn, byte_t Rd, word_t *registers, byte_t *memory) {
-	registers[Rn] = memory[registers[Rd]];
-}
-
-static void store(byte_t Rn, byte_t Rd, word_t *registers, byte_t *memory) {
-	memory[registers[Rd]] = registers[Rn];
-}
-
-static void execute_pre_indexing(byte_t *firstByte, 
-	word_t *registers, byte_t*memory
-) {
+// Offset added/subtracted to the base register before transferring the data.
+static void execute_pre_indexing(
+		byte_t *firstByte, 
+		word_t *registers, 
+		byte_t*memory) 
+{
 	byte_t Rn = get_Rn(firstByte[1]);
 
-	if (get_up_bit(firstByte[1])) {
-		Rn = add_offset(
-				Rn, 
-				get_offset(firstByte[2], firstByte[3], immediate_operand(firstByte[0]), registers) 
-		);	
-	} else {
-		Rn = subtract_offset(
-				Rn, 
-				get_offset(firstByte[2], firstByte[3], immediate_operand(firstByte[0]), registers)
-		);	
-	}
+	Rn = apply_offset(Rn, firstByte, registers);
 
 	if (get_load_store_bit(firstByte[1])) {
 		load(Rn, get_Rd(firstByte[2]), registers, memory);
@@ -76,6 +88,7 @@ static void execute_pre_indexing(byte_t *firstByte,
 	}
 }
 
+// Offset added/subtracted to the register after transferring the data.	
 static void execute_post_indexing(byte_t *firstByte, word_t *registers,
  byte_t *memory
 ) {
@@ -87,36 +100,85 @@ static void execute_post_indexing(byte_t *firstByte, word_t *registers,
 		store(Rn, get_Rd(firstByte[2]), registers, memory);
 	}
 
+	change_base_register_by_offset(Rn, firstByte, registers);
+}
+
+// Used in case of pre-indexing. Offset is applied and new register address is returned.
+// The original register remains unchanged.
+static byte_t apply_offset(byte_t Rn, byte_t *firstByte, word_t *registers) {
+	// Offset is added when Up bit is set.
 	if (get_up_bit(firstByte[1])) {
+
+		Rn = add_offset(
+				Rn, 
+				get_offset(
+						firstByte[2], 
+						firstByte[3], 
+						immediate_operand(firstByte[0]), 
+						registers) 
+		);	
+
+	} else {
+
+		Rn = subtract_offset(
+				Rn, 
+				get_offset(
+						firstByte[2], 
+						firstByte[3], 
+						immediate_operand(firstByte[0]), 
+						registers)
+		);	
+
+	}
+	return Rn;
+}
+
+static void change_base_register_by_offset(
+		byte_t Rn, 
+		byte_t *firstByte, 
+		word_t *registers) 
+{
+	// Offset is added when Up bit is set.
+	if (get_up_bit(firstByte[1])) {
+
 		registers[Rn] = add_offset(
 				Rn, 
-				get_offset(firstByte[2], firstByte[3], immediate_operand(firstByte[0]), registers)
+				get_offset(
+						firstByte[2], 
+						firstByte[3], 
+						immediate_operand(firstByte[0]), 
+						registers)
 		);	
+
 	} else {
+
 		registers[Rn] = subtract_offset(
 				Rn, 
-				get_offset(firstByte[2], firstByte[3], immediate_operand(firstByte[0]), registers)
+				get_offset(
+						firstByte[2], 
+						firstByte[3], 
+						immediate_operand(firstByte[0]), 
+						registers)
 		);	
-	}
 
-}
-
-
-
-void execute_single_data_transfer (byte_t *firstByte, word_t *registers, byte_t *memory) {
-	if (get_pre_post_indexing_bit(firstByte[1])) {
-
-		// Offset added/subtracted to the base register before transferring the data.
-		execute_pre_indexing(firstByte, registers, memory);
-
-	} else {
-
-		// Offset added/subtracted to the register after transferring the data.	
-		execute_post_indexing(firstByte, registers, memory);
 	}
 }
 
+static void load(byte_t Rn, byte_t Rd, word_t *registers, byte_t *memory) {
+	registers[Rn] = memory[registers[Rd]];
+}
 
+static void store(byte_t Rn, byte_t Rd, word_t *registers, byte_t *memory) {
+	memory[registers[Rd]] = registers[Rn];
+}
+
+static byte_t add_offset(byte_t Rn, byte_t offset) {
+	return Rn + offset;	
+}
+
+static byte_t subtract_offset(byte_t Rn, byte_t offset) {
+	return Rn - offset;
+}
 
 int main(void) {
 
