54e7a57ce94045ca3d994b216c51d409a55887cf
Oscar Garcia Velasco Newell <og519@texel01.doc.ic.ac.uk>
2021-05-27 10:53:21 +0100

    Adding typedefs for clearer code moving forward

--- src/defns.h
+++ src/defns.h
@@ -2,11 +2,14 @@
 #define DEFN
 
 /*
- * This file contains all of the definitions used accross the project.
+ * Type Definitions for types used across project
  */
 
 typedef unsigned char byte_t;
 typedef unsigned long int word_t;
+typedef unsigned char nibble_t;
+typedef unsigned char bit_t;
+typedef unsigned long int register_t;
 
 #endif
 
