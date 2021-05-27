7c12ed2f4e49837f837d53b6bae89ed9b3fd532e
Oscar Garcia Velasco Newell <og519@texel01.doc.ic.ac.uk>
2021-05-27 10:32:49 +0100

    Branch Header File Complete + Execute Branch Function Partially Completed

--- src/emulator/branch/branch.c
+++ src/emulator/branch/branch.c
@@ -1,3 +1,17 @@
+#include <stdio.h> 
+#include "../../defns.h"
+#include "binaryString.h"
+#include "../common_functions/common.h"
+
+word_t get_offest (byte_t secondByte) {
+	return (secondByte[0] << 16) | (secondByte[1] << 8) | secondByte[2];
+}
+
+void execute_branch (byte_t *firstByte, word_t *registers) {
+	word_t offset = get_offset(firstByte[1]); 
+	// TO DO: Branch Execution Implementation
+}
+
 int main(void) {
-	// Dummy for compilation.
+	return 0; 
 }
