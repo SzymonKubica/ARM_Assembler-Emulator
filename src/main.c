a4ce1f2e7b4940e9a5bf3d5f71c7b54bbab7af49
Wu, Haotian <haotian.wu20@imperial.ac.uk>
2021-05-28 17:20:10 +0100

    removed unneccessary files

--- src/main.c
+++
@@ -1,7 +0,0 @@
-#include "cond.h"
-#include <stdio.h>
-
-int main(void) {
-	printf("%d \n", checkCond(0b1110));
-	return 0;
-}
--- src/test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-
-int main (void) {
-	printf("%d", sizeof(uint32_t));
-}
