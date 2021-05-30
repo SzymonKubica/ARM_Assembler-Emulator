d3b3cdfb79c79681952f372715e83caa0502424a
Wu, Haotian <haotian.wu20@imperial.ac.uk>
2021-05-30 17:06:56 +0100

    fixed common.c shifter - error in carry

--- src/emulator/common_functions/common.c
+++ src/emulator/common_functions/common.c
@@ -15,16 +15,16 @@ byte_t get_Second_Nibble (byte_t byte) {
 word_t shifter (byte_t shiftType, byte_t shiftAmount, word_t word, bit_t *carry) {
 	switch (shiftType){
 		case 0: // logical left
-			*carry = (((word << shiftAmount) - 1) >> 31) & 1; 
+			*carry = ((word << (shiftAmount - 1)) >> 31) & 1; 
 			return word << shiftAmount;
 		case 1: // logical right
-			*carry = ((word >> shiftAmount) - 1) & 1; 
+			*carry = ((word >> (shiftAmount - 1))) & 1; 
 			return word >> shiftAmount;
 		case 2: // arithmetic right
-			*carry = ((word >> shiftAmount) -1) & 1; 
+			*carry = ((word >> (shiftAmount - 1))) & 1; 
 			return word / (1 << shiftAmount);
 		case 3: // rotate right
-			*carry = ((word >> shiftAmount) -1) & 1; 
+			*carry = ((word >> (shiftAmount - 1))) & 1; 
 			return  (word >> shiftAmount) | (word << (32 - shiftAmount));
 		default: // ERROR: Should never be reached
 			return -1;
