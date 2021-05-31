2e8a7ffb319eb7813e6fe5302f3710b80fac2f56
Szymon Kubica <szymon.kubica20@imperial.ac.uk>
2021-05-31 07:40:26 +0200

    Added an early draft for branch implementation.

--- src/emulator/branch/branch.c
+++ src/emulator/branch/branch.c
@@ -3,15 +3,31 @@
 #include "binaryString.h"
 #include "../common_functions/common.h"
 
-/*
+#define PC 15
+
 static word_t get_offset (byte_t *secondByte) {
 	return ((secondByte[0] << 16) | (secondByte[1] << 8)) | secondByte[2];
 }
-*/
+
+static word_t get_sign_extended_offset (word_t offset) {
+	// Pre: After shifting offset is precisely 26 bits long.
+  if (offset >> 24) { // If the most significant bit is non-zero.
+
+		// We copy the leftmost bit 8 times.
+		word_t filler = readBinaryWord("1111 1100 0000 0000 0000 0000 0000 0000");
+		return filler | offset;
+	} else {
+
+		// The leftmost bit is zero, so adding it 8 times doesn't alter the offset.
+		return offset;
+	}
+}
 
 void execute_branch (byte_t *firstByte, word_t *registers) {
-	//word_t offset = get_offset(firstByte); 
-	// TO DO: Branch Execution Implementation
+	word_t offset = get_offset(firstByte); 
+	offset = offset << 2;
+	word_t extended_offset = get_sign_extended_offset(offset);
+	registers[PC] += extended_offset;
 }
 
 
--- src/emulator/common_functions/common.c
+++ src/emulator/common_functions/common.c
@@ -11,7 +11,6 @@ byte_t get_Second_Nibble (byte_t byte) {
 	return byte & readBinary("1111");
 }
 
-
 word_t shifter (byte_t shiftType, byte_t shiftAmount, word_t word, bit_t *carry) {
 	switch (shiftType){
 		case 0: // logical left
