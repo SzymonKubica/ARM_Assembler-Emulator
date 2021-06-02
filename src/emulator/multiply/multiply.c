8b93b5dd817782e6b7b0a10d17118f84a29fe8c2
Szymon Kubica <szymon.kubica20@imperial.ac.uk>
2021-06-02 08:40:41 +0200

    Multiply.c has been restructured. Custom typedefs were substituted. Reduced the complexity and number of function calls.

--- src/emulator/multiply/multiply.c
+++ src/emulator/multiply/multiply.c
@@ -2,49 +2,55 @@
 #include "../../defns.h"
 #include "common.h"
 
-byte_t get_Rn_multiply(byte_t thirdByte) {
+static nibble_t get_Rn_multiply(byte_t thirdByte) {
 	return get_First_Nibble(thirdByte); 
 }
 
-byte_t get_Rd_multiply(byte_t secondByte) {
+static nibble_t get_Rd_multiply(byte_t secondByte) {
 	return get_Second_Nibble(secondByte);
 }
 
-byte_t get_Rs(byte_t thirdByte) {
+static nibble_t get_Rs(byte_t thirdByte) {
 	return get_Second_Nibble(thirdByte); 
 }
 
-byte_t get_Rm(byte_t fourthByte) {
+static nibble_t get_Rm(byte_t fourthByte) {
 	return get_Second_Nibble(fourthByte);
 }
 
-byte_t get_A(byte_t secondByte) {
+static bit_t get_A(byte_t secondByte) {
 	return get_First_Nibble(secondByte) >> 1;
 }
 
-byte_t get_S(byte_t secondByte) {
+static bit_t get_S(byte_t secondByte) {
 	return get_First_Nibble(secondByte) & 1;
 }
 
-void set_CPSR(word_t result, word_t *cpsr) {
+static void set_CPSR(word_t result, word_t *cpsr) {
 	*cpsr &= 0x7fffffff;
 	*cpsr |= (result >> 31) << 31;
+
 	if(!result) {
-        	*cpsr |= 0x40000000;
+		*cpsr |= 0x40000000;
 	}	       
 }
 
 void execute_multiply(byte_t *firstByte, word_t *registers) {
+
+	nibble_t Rd = get_Rd_multiply(firstByte[1]); 
+	nibble_t Rs = get_Rs(firstByte[2]);
+	nibble_t Rm = get_Rm(firstByte[3]);
+
 	if (get_A(firstByte[1])) {
-		registers[get_Rd_multiply(firstByte[1])] 
-			= registers[get_Rm(firstByte[3])] * registers[get_Rs(firstByte[2])]
-			 + registers[get_Rn_multiply(firstByte[2])];
+		nibble_t Rn = get_Rn_multiply(firstByte[2]);
+
+		registers[Rd] = registers[Rm] * registers[Rs] + registers[Rn];
 	} else {
-		registers[get_Rd_multiply(firstByte[1])]
-			= registers[get_Rm(firstByte[3])] * registers[get_Rs(firstByte[2])]; 
+
+		registers[Rd] = registers[Rm] * registers[Rs]; 
 	}
 	
 	if (get_S(firstByte[1])) {
-		set_CPSR(registers[get_Rd_multiply(firstByte[1])], &registers[16]);
+		set_CPSR(registers[Rd], &registers[16]);
 	}
 }
